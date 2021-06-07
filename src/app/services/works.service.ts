d1b27a6a9d8b7e40ee690bb3c674038b6c4ea1e1
Andy Macias <xandyx2014@gmail.com>
2021-06-07 00:40:14 -0400

    add work

--- src/app/services/works.service.ts
+++ src/app/services/works.service.ts
@@ -1,72 +1,90 @@
-import { Work } from "../models/works.model";
+import { Work } from '../models/works.model';
 
-export class WorkService{
-    work:Work[]=[
-        {
-            title: 'AdoptionFinder',
-            description: 'Platform for animal adoption has a front part for customers and an administrative part where you can generate reports. It has modules, user, adoption, blog, publication, adoption management, pet tracking.',
-            link: 'https://github.com/xandyx2014/Adoption-finder/blob/main/README.md'
-        },
-        {
-            title: 'Bobino Control',
-            description: 'Application for bovine fattening and fattening of dairy cows where the calculation history is saved for future previews, it was made with the Ge',
-            link: 'https://github.com/xandyx2014/control_bobino_app/blob/master/README.md'
-        },
-        {
-            title: 'MovieAppReact',
-            description: 'Application created in react native consuming The moviedb api using react native paper for the UI components. .',
-            link: 'https://github.com/xandyx2014/My-Movie-app/blob/main/README.md'
-        },
-        {
-            title: 'Instant-Gaming-copy',
-            description: 'Platform inspired by instant gaming where you can create your games categories of games users order, order management, has a search engine to facilitate finding the desired game.',
-            link: 'https://github.com/xandyx2014/instant-gaming-copy/blob/main/README.md'
-        },
-        {
-            title: 'Q&Q',
-            description: 'Application to order electrical goods where you have your cart, user and you can place your orders online just by adding your current location.',
-            link: 'https://github.com/xandyx2014/Q-Q-app/blob/main/README.md'
-        },
-       
-        {
-            title: 'Calculate nutritional',
-            description: 'Application for customer care, nutrition control and food control and delivery of food in a certain amount of time and information for health care. ',
-            link: 'https://github.com/xandyx2014/Calculadora-nutricional/blob/master/readme.md'
-        },
-        {
-            title: 'App Olivos',
-            description: 'Application for customer care, nutrition control and food control and delivery of food in a certain amount of time and information for health care.',
-            link: 'https://github.com/xandyx2014/Control-nutricional-Frontend-App/blob/master/README.md'
-        },
-        {
-            title: 'DIETANUT',
-            description: 'Application for the creation of diets performing two calculations: Chemical composition of the diet and food exchange where you can calculate them respectively and export them in PDF format.',
-            link: 'https://github.com/xandyx2014/Control-de-dieta-app/blob/main/README.md'
-        },
-        {
-            title: 'MicroKid',
-            description: 'Child care education application for pregnant or expectant mothers includes a reminder to pick up the groceries given by the government.',
-            link: 'https://github.com/xandyx2014/microkitApp/blob/master/README.md'
-        },
-        {
-            title: 'Party plus',
-            description: 'Application to generate tokens to pass them to your guests, where you can set the limit of uses expiration time, once spent the token is not valid, the token is not valid',
-            link: 'https://github.com/xandyx2014/app-qr-party/blob/main/README.md'
-        },
-        {
-            title: 'Bids App',
-            description: 'Simple application for music albums',
-            link: 'https://github.com/xandyx2014/Bids-app/blob/main/README.md'
-        },
-         {
-            title: 'Qr app Detector',
-            description: 'QR application for personnel access - this is the part of the security guard who will verify that the QR generated is valid or not.',
-            link: 'https://github.com/xandyx2014/Qr.guard-detector/blob/main/README.md'
-        },
-        {
-            title: 'Qr app Cliente ',
-            description: 'QR application for personnel access this is the part that generates an invitation QR to give access to the guest and then the security guard will verify that the generated QR is valid or not.',
-            link: 'https://github.com/xandyx2014/Qr-Guard-cliente/blob/main/README.md'
-        },
-    ];
+export class WorkService {
+  work: Work[] = [
+    {
+      title: 'AdoptionFinder',
+      description:
+        'Platform for animal adoption has a front part for customers and an administrative part where you can generate reports. It has modules, user, adoption, blog, publication, adoption management, pet tracking.',
+      link: 'https://github.com/xandyx2014/Adoption-finder/blob/main/README.md',
+    },
+    {
+      title: 'HemoVirtual',
+      description:
+        "Information platform of the patient's medical consultations, where you can see your consultations depending on which category it is.",
+      link: 'https://github.com/xandyx2014/Hemovirtualscreen/blob/main/readme.md',
+    },
+    {
+      title: 'Bobino Control',
+      description:
+        'Application for bovine fattening and fattening of dairy cows where the calculation history is saved for future previews, it was made with the Ge',
+      link: 'https://github.com/xandyx2014/control_bobino_app/blob/master/README.md',
+    },
+    {
+      title: 'MovieAppReact',
+      description:
+        'Application created in react native consuming The moviedb api using react native paper for the UI components. .',
+      link: 'https://github.com/xandyx2014/My-Movie-app/blob/main/README.md',
+    },
+    {
+      title: 'Instant-Gaming-copy',
+      description:
+        'Platform inspired by instant gaming where you can create your games categories of games users order, order management, has a search engine to facilitate finding the desired game.',
+      link: 'https://github.com/xandyx2014/instant-gaming-copy/blob/main/README.md',
+    },
+    {
+      title: 'Q&Q',
+      description:
+        'Application to order electrical goods where you have your cart, user and you can place your orders online just by adding your current location.',
+      link: 'https://github.com/xandyx2014/Q-Q-app/blob/main/README.md',
+    },
+
+    {
+      title: 'Calculate nutritional',
+      description:
+        'Application for customer care, nutrition control and food control and delivery of food in a certain amount of time and information for health care. ',
+      link: 'https://github.com/xandyx2014/Calculadora-nutricional/blob/master/readme.md',
+    },
+    {
+      title: 'App Olivos',
+      description:
+        'Application for customer care, nutrition control and food control and delivery of food in a certain amount of time and information for health care.',
+      link: 'https://github.com/xandyx2014/Control-nutricional-Frontend-App/blob/master/README.md',
+    },
+    {
+      title: 'DIETANUT',
+      description:
+        'Application for the creation of diets performing two calculations: Chemical composition of the diet and food exchange where you can calculate them respectively and export them in PDF format.',
+      link: 'https://github.com/xandyx2014/Control-de-dieta-app/blob/main/README.md',
+    },
+    {
+      title: 'MicroKid',
+      description:
+        'Child care education application for pregnant or expectant mothers includes a reminder to pick up the groceries given by the government.',
+      link: 'https://github.com/xandyx2014/microkitApp/blob/master/README.md',
+    },
+    {
+      title: 'Party plus',
+      description:
+        'Application to generate tokens to pass them to your guests, where you can set the limit of uses expiration time, once spent the token is not valid, the token is not valid',
+      link: 'https://github.com/xandyx2014/app-qr-party/blob/main/README.md',
+    },
+    {
+      title: 'Bids App',
+      description: 'Simple application for music albums',
+      link: 'https://github.com/xandyx2014/Bids-app/blob/main/README.md',
+    },
+    {
+      title: 'Qr app Detector',
+      description:
+        'QR application for personnel access - this is the part of the security guard who will verify that the QR generated is valid or not.',
+      link: 'https://github.com/xandyx2014/Qr.guard-detector/blob/main/README.md',
+    },
+    {
+      title: 'Qr app Cliente ',
+      description:
+        'QR application for personnel access this is the part that generates an invitation QR to give access to the guest and then the security guard will verify that the generated QR is valid or not.',
+      link: 'https://github.com/xandyx2014/Qr-Guard-cliente/blob/main/README.md',
+    },
+  ];
 }
